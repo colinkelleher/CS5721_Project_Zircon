c9f02a1a28048bff4d72d68024e43dcf1734e8f2
Colin Kelleher <21160562@studentmail.ul.ie>
2021-11-01 22:49:41 +0000

    LogOpenListener

---
+++ src/main/java/CS5721/project/observer/listeners/LogOpenListener.java
@@ -0,0 +1,16 @@
+package CS5721.project.observer.listeners;
+
+import java.io.File;
+
+public class LogOpenListener implements EventListener {
+    private File log;
+
+    public LogOpenListener(String fileName) {
+        this.log = new File(fileName);
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Save to log" + log + eventType + file.getName());
+    }
+}
