33f4fa1ed4a5583fb3ecc07f2b82ceaa3a37b3d3
ThibLMT <21165823@studentmail.ul.ie>
2021-11-01 23:07:22 +0000

    Add getter for CalendarEvents

--- src/main/java/CS5721/project/entity/calendar/Calendar.java
+++ src/main/java/CS5721/project/entity/calendar/Calendar.java
@@ -5,5 +5,9 @@ import CS5721.project.entity.calendar.CalendarEvent;
 import java.util.List;
 
 public class Calendar {
+    public List<CalendarEvent> getEvents() {
+        return events;
+    }
+
     private List<CalendarEvent> events;
 }
