d00cbdc1cf23248e5035607184b9a2bfca50cf25
Colin Kelleher <21160562@studentmail.ul.ie>
2021-11-04 14:53:02 +0000

    Creating test for eventManager

---
+++ src/test/java/CS5721/project/Observer/EventManagerTest.java
@@ -0,0 +1,12 @@
+package CS5721.project.Observer;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EventManagerTest {
+
+    @Test
+    public void eventManagerTest (){}
+
+}
