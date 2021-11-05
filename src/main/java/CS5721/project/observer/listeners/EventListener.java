f1d04071abc1ded3e971852a59e16f108ca06be1
Colin Kelleher <21160562@studentmail.ul.ie>
2021-11-05 01:23:31 +0000

    Removing redundant import

--- src/main/java/CS5721/project/observer/listeners/EventListener.java
+++ src/main/java/CS5721/project/observer/listeners/EventListener.java
@@ -2,8 +2,6 @@ package CS5721.project.observer.listeners;
 
 import CS5721.project.entity.calendar.CalendarEvent;
 
-import java.io.File;
-
 public interface EventListener {
 
     void update (String eventType, CalendarEvent event, Long employeeID);
