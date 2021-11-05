f3a8cae4e92c0594f9aa3d405c8c4864843fe750
ThibLMT <21165823@studentmail.ul.ie>
2021-11-05 01:11:07 +0000

    Add basis for testing EventManager

--- src/test/java/CS5721/project/Observer/EventManagerTest.java
+++ src/test/java/CS5721/project/Observer/EventManagerTest.java
@@ -1,12 +1,16 @@
 package CS5721.project.Observer;
 
 
+import CS5721.project.observer.publisher.EventManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class EventManagerTest {
 
     @Test
-    public void eventManagerTest (){}
+    public void eventManagerTest (){
+        EventManager eventManager = new EventManager("add_event","create_request");
+
+    }
 
 }
