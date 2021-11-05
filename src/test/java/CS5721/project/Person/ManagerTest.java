b50e13fb642cf4d071554b0989b2870376881897
Colin Kelleher <21160562@studentmail.ul.ie>
2021-11-05 01:59:17 +0000

    Creating some simple test cases for Manager

---
+++ src/test/java/CS5721/project/Person/ManagerTest.java
@@ -0,0 +1,25 @@
+package CS5721.project.Person;
+
+import CS5721.project.entity.calendar.Calendar;
+import CS5721.project.entity.person.Employee;
+import CS5721.project.entity.wage.Wage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ManagerTest {
+    Employee new_manager = new Employee(1223544L, "Test Manager", new Wage(), new Calendar());
+
+    @Test
+    public void testManagerID() {
+        long managerID = new_manager.getId();
+        // Simple assertion to get ID
+        Assertions.assertEquals(1223544,managerID);
+    }
+
+    @Test
+    public void testManagerName() {
+        String managerName = new_manager.getName();
+        // Simple assertion to get ID
+        Assertions.assertEquals("Test Manager",managerName);
+    }
+}
