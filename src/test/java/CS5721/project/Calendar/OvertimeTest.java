e3c14a03b6654b1bee11c984da54caaba5ce8c58
Colin Kelleher <21160562@studentmail.ul.ie>
2021-11-07 14:55:49 +0000

    OvertimeTest

---
+++ src/test/java/CS5721/project/Calendar/OvertimeTest.java
@@ -0,0 +1,39 @@
+package CS5721.project.Calendar;
+
+
+import CS5721.project.entity.calendar.Calendar;
+import CS5721.project.entity.calendar.CalendarEvent;
+import CS5721.project.entity.calendar.OvertimeEvent;
+import CS5721.project.entity.person.Employee;
+import CS5721.project.entity.wage.Wage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class OvertimeTest {
+    Employee new_employee = new Employee(122354L, "Test Employee", new Wage(), new Calendar());
+    public static final LocalDateTime EVENT_START_DATE = LocalDateTime.of(2021, 11, 5, 9, 0);
+    public static final LocalDateTime EVENT_END_DATE = LocalDateTime.of(2021, 11, 5, 18, 30);
+    Calendar calendar1 = new_employee.getCalendar();
+    CalendarEvent overtimeEvent = new OvertimeEvent(EVENT_START_DATE, EVENT_END_DATE);
+
+    @Test
+    public void overtimeTest(){
+        new_employee.setCalendar(calendar1);
+        calendar1.addEvent(overtimeEvent);
+        ArrayList<CalendarEvent> events = calendar1.getEvents();
+        Assertions.assertTrue(overtimeEvent.isOvertime());
+    }
+    @Test
+    public void overtimePayTest(){
+        Assertions.assertNotEquals(0,overtimeEvent.getPay());
+    }
+
+    @Test
+    public void overtimeIsTrainingTest (){
+        Assertions.assertFalse(overtimeEvent.isTraining());
+    }
+
+}
