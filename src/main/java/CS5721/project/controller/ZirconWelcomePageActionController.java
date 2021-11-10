dad455948b4cabddc50c2469b460db6512a12296
Benjamin <21157146@studentmail.ul.ie>
2021-11-10 16:47:55 +0000

    Link between Clocking page and clocking confirmation

--- src/main/java/CS5721/project/controller/ZirconWelcomePageActionController.java
+++ src/main/java/CS5721/project/controller/ZirconWelcomePageActionController.java
@@ -1,19 +1,20 @@
 package CS5721.project.controller;
 
-import CS5721.project.entity.person.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import CS5721.project.entity.person.Employee;
+
 @Controller
 @RequestMapping(path = "")
 public class ZirconWelcomePageActionController {
 
-    @PostMapping(path = "/")
-    public String clock() {
+    @PostMapping(path = "/clocking")
+    public String clocking(@ModelAttribute Employee employee) {
 
-        return "redirect:" + "/clockingConfirmation";
+        return "redirect:" + "/clockingConfirmation?employeeId=" + employee.getId();
 
     }
 }
