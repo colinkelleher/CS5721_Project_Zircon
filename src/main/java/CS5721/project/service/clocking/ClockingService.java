8cfc67da61dd8b0250ff0467e7e993be883dd398
ThibLMT <21165823@studentmail.ul.ie>
2021-11-11 13:52:06 +0000

    Forgot comment oopsie

--- src/main/java/CS5721/project/service/clocking/ClockingService.java
+++ src/main/java/CS5721/project/service/clocking/ClockingService.java
@@ -10,6 +10,7 @@ public class ClockingService implements IClockingService{
     @Override
     public CalendarEvent[] getClockingEvents(ClockingInfo clockingInfo, Shift shift) {
         CalendarEvent[] eventsArray = new CalendarEvent[]{};
+        //TODO implement calculation of events from a shift and clocking info, i.e how many Regular or Overtime Events we return
         return new CalendarEvent[0];
     }
 }
