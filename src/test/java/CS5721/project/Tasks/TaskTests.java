01b97be0b5215f5b8221f67590ea5f1b9479a417
Colin Kelleher <21160562@studentmail.ul.ie>
2021-11-13 13:25:41 +0000

    Addition of ID test and Update ID test

--- src/test/java/CS5721/project/Tasks/TaskTests.java
+++ src/test/java/CS5721/project/Tasks/TaskTests.java
@@ -6,9 +6,13 @@ import CS5721.project.tasks.TaskDAOimpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 
 public class TaskTests {
     TaskDAO taskDAO = new TaskDAOimpl();
+    Task task4 = new Task(44,"Refactoring","Refactor lines 1-33","Ewen",1L, LocalDateTime.of(2021,11,15,11,30,0),LocalDateTime.of(2021,11,15,14,30,0));
+
 
     @Test
     public void TaskIdTest(){
@@ -20,6 +24,19 @@ public class TaskTests {
     public void TaskEmployeeTest(){
         Task task = taskDAO.getTask(1);
         Assertions.assertEquals("Colin",task.getTeamEmployee());
+    }
+
+    @Test
+    public void TaskEmployeeIdTest(){
+        Task taskIdEmp = taskDAO.getTask(1);
+        Assertions.assertEquals(1,taskIdEmp.getEmployeeId());
+    }
+
+    @Test
+    public void TaskUpdateEmployee(){
+        Task taskupdate = taskDAO.getTask(1);
+        taskupdate.setEmployeeId(2);
+        Assertions.assertEquals(2,taskDAO.getTask(1).getEmployeeId());
     }
 
 }
